5d0c5632e3e8a88742b1c485869e3a881658418d
EuiHyeok <dmlgur0517@daum.net>
2024-08-02 08:57:37 +0900

    [JAVA] Algorithm

--- algorithm/src/test/java/com/ohgiraffers/chap06/section01/dp/Application4Tests.java
+++ algorithm/src/test/java/com/ohgiraffers/chap06/section01/dp/Application4Tests.java
@@ -13,35 +13,33 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class Application3Tests {
-
-    private static Integer input1, input2;
-    private static Integer output1,output2;
+public class Application4Tests {
+    private static String input1;
+    private static Integer output1;
     @BeforeAll
     public static void set() {
 
         /* 예시1 */
-        input1 = 6;
-        output1 = 3;
-
-        /* 예시2 */
-        input2 = 12;
-        output2 = 16;
+        input1 = "5\n" +
+                "7\n" +
+                "3 8\n" +
+                "8 1 0\n" +
+                "2 7 4 4\n" +
+                "4 5 2 6 5";
+        output1 = 30;
     }
 
     public static Stream<Arguments> provideSource() {
         return Stream.of(
-                arguments(input1, output1),
-                arguments(input2, output2)
+                arguments(input1, output1)
         );
     }
-    @DisplayName("dp3")
+    @DisplayName("dp4")
     @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
     @ParameterizedTest
     @MethodSource("provideSource")
-    public void dp3test(Integer input, Integer output) throws Exception {
-        Integer result = Application3.solution(input);
+    public void dp4test(String input, Integer output) throws Exception {
+        Integer result = Application4.solution(input);
         Assertions.assertEquals(output, result);
     }
 }
-
